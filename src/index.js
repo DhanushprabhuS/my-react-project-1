6f297f13730bf9d8287a69f69e847bbae08a01e2
DhanushprabhuS <dhanushprabhu.19it@sonatech.ac.in>
2022-09-16 21:18:42 +0530

    Deployed website hash-routed

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { HashRouter } from 'react-router-dom';
+import HashRouter from 'hash-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+    <App />
     </HashRouter>
   </React.StrictMode>
 );
